3c8a3ddf9caa94472d2407b6265133e289fb0120
YOUPIDOK <58979097+YOUPIDOK@users.noreply.github.com>
2023-07-12 12:08:13 +0200

    documentation

--- client/src/api/authApi.ts
+++ client/src/api/authApi.ts
@@ -2,10 +2,13 @@ import axios from "axios";
 import moment from "moment";
 
 const urn = 'http://localhost:3000/V1/auth';
-export async function searchRecipe(query: string) {
-    return await axios.get(urn, {params: {search: query}}).then((response) => { return response.data });
-}
 
+/**
+ * User login
+ * Returns user and tokens or error message
+ * @param email
+ * @param password
+ */
 export async function login(email, password) {
     return await axios.post(urn + '/login', {email: email, password: password})
         .then((res) => res.data)
